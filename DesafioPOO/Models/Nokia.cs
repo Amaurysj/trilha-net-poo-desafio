1fe2add1da3939f701665b6d3def448a9a2540d6
Amaurysj <amaury-developer@outlook.com>
2022-10-31 04:07:40 -0300

    UP
    Desafio DIO - Criando um Sistema e Abstraindo um Celular com POO em C#

---
+++ DesafioPOO/Models/Nokia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
+    {
+
+        public Nokia(string numero, string modelo, string IMEI, int memoria) : base(numero, modelo, IMEI, memoria)
+        {
+            
+        }
+
+        public string Numero { get; }
+        public string Modelo { get; }
+        public string IMEI { get; }
+        public int Memoria { get; }
+
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Telefone Nokia Instalando aplicativo: {nomeApp}");
+        }
+    }
+}
---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using DesafioPOO.Models;
+using System;
+
+namespace DesafioPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Realizar os testes com as classes Nokia e Iphone
+            Console.WriteLine("\nTelefone Nokia");
+            Smartphone nokia = new Nokia(numero: "94545-5555", modelo: "C20", IMEI: "561536030195001", memoria: 32);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Facebook");
+            nokia.ReceberLigacao();
+
+            Console.WriteLine("\nTelefone Iphone");
+            Smartphone iphone = new Iphone(numero: "94545-4444", modelo: "13 Pro Max", IMEI: "561536430195011", memoria: 64);
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Instagram");
+            iphone.ReceberLigacao();
+        }
+    }
+}
